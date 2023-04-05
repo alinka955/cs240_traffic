9427d00daea67f41ced7e7c98cc46a30a163cb44
alinka955 <78833495+alinka955@users.noreply.github.com>
2023-04-05 19:50:48 -0400

    a method to create a dictionary based on input.txt

--- intersection.cpp
+++ intersection.cpp
@@ -1,13 +1,61 @@
 #include <iostream>
 #include "Animator.h"
 #include "VehicleBase.h"
-#include "readinput.cpp"
 #include <vector>
 
-// import dictionary
-map<string,double> inputDict{readinput.inputToDict(argv[1])};
+void error(string err, string arg) {
+        cerr << err << arg << endl;
+        exit(0);
+} 
 
-int halfsize = number_of_sections_before_intersection;
+// dictionary input
+map<string, double> inputToDict(string input){
+
+        //Open the input file
+        ifstream infile{input};
+
+        //Check if infile cannot be open
+        if (!infile.is_open()){
+                error("Unable to open the following file: ", input);
+        }
+        string parameter;
+        double value;
+   
+        //Create a dictionary to store values
+        map<string, double> dict;
+        while (infile >> parameter >> value){
+                dict.insert({parameter, value});
+        }
+        infile.close();
+        return dict;
+}
+
+// find the values of each variable, read from input
+double halfsize = inputDict["number_of_sections_before_intersection"];
+double maximum_simulated_time = inputDict["maximum_simulated_time"]
+
+double green_east_west = inputDict["green_east_west"]
+double green_north_south = inputDict["green_north_south"]
+
+double prob_new_vehicle_eastbound = inputDict["prob_new_vehicle_eastbound"]
+double prob_new_vehicle_northbound = inputDict["prob_new_vehicle_northbound"]
+double prob_new_vehicle_southbound = inputDict["prob_new_vehicle_southbound"]
+double prob_new_vehicle_westbound = inputDict["prob_new_vehicle_westbound"]
+
+double proportion_of_SUVs = inputDict["proportion_of_SUVs"]
+double proportion_of_cars = inputDict["proportion_of_cars"]
+
+double proportion_right_turn_SUVs = inputDict["proportion_right_turn_SUVs"]
+double proportion_right_turn_cars = inputDict["proportion_right_turn_cars"]
+double proportion_right_turn_trucks = inputDict["proportion_right_turn_trucks"]
+
+double yellow_east_west = inputDict["yellow_east_west"]
+double yellow_north_south = inputDict["yellow_north_south"]
+
+double proportion_left_turn_SUVs = inputDict["proportion_left_turn_SUVs"]
+double proportion_left_turn_cars = inputDict["proportion_left_turn_cars"]
+double proportion_left_turn_trucks = inputDict["proportion_left_turn_trucks"]
+
 
 Animator anim(halfSize);
 
@@ -19,20 +67,21 @@ std::vector<VehicleBase*> northbound(halfSize * 2 + 2, nullptr);
 
 // random number generator
 
-
 if (prob_new_vehicle_northbound) {
    if (proportion_of_cars) {
       VehicleBase v(VehicleType::car, Direction::NorthBound); 
    }
    else if (proportion_of_SUVs){
-     VehicleBase v(VehicleType::suv, Direction::NorthBound); 
+      VehicleBase v(VehicleType::suv, Direction::NorthBound); 
    }
-  else{
-    VehicleBase v(VehicleType::truck, Direction::NorthBound); 
+   else{
+      VehicleBase v(VehicleType::truck, Direction::NorthBound); 
   }
 }
 
 
 int main (int argc, char* argv[]){
+   // get dictionary with input
+   map<string,double> inputDict{inputToDict(argv[1])};
    
 }
