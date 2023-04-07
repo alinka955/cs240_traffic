eaf44a06f08a2abbfd2a42ea9f74caae18e58fec
danielkatz19 <98559516+danielkatz19@users.noreply.github.com>
2023-04-07 17:40:13 -0400

    Create intersection.h

---
+++ intersection.h
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "Animator.h"
+#include "VehicleBase.h"
+//#include "Random.cpp"
+#include <vector>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include"readinput.cpp"
+
+using namespace std;
+
+class Intersection{
+private:
+    static map<string, double> inputDict;
+    double halfsize;
+    double maximum_simulated_time;
+    double green_east_west;
+    double green_north_south;
+    double prob_new_vehicle_eastbound;
+    double prob_new_vehicle_northbound;
+    double prob_new_vehicle_southbound;
+    double prob_new_vehicle_westbound;
+    double proportion_of_SUVs;
+    double proportion_of_cars;
+    double proportion_right_turn_SUVs;
+    double proportion_right_turn_cars;
+    double proportion_right_turn_trucks;
+
+    double yellow_east_west;
+    double yellow_north_south;
+
+    double proportion_left_turn_SUVs;
+    double proportion_left_turn_cars;
+    double proportion_left_turn_trucks;
+
+    void checkCarSpawn();
+    int assignVehicle();
+public:
+    Intersection(const map<string, double>);
+    Intersection(const Intersection& other);
+    Intersection& operator=(Intersection& other);
+    Intersection(Intersection&& other);
+    Intersection& operator=(Intersection&& other);
+    ~Intersection();
+};
