c31ead24aac17f6f1ba3ba7c8fb34ba09625ff54
danielkatz19 <98559516+danielkatz19@users.noreply.github.com>
2023-04-11 12:01:07 -0400

    Update intersection.h

--- intersection.h
+++ intersection.h
@@ -7,13 +7,28 @@
 #include <sstream>
 #include <fstream>
 #include"readinput.h"
+#include <string>
 
-using namespace std;
+// Forward declarations
 
-class Intersection{
+class Intersection {
+public:
+    Intersection();
+    Intersection(const Intersection& other);
+    Intersection& operator=(Intersection& other);
+    Intersection(Intersection&& other);
+    Intersection& operator=(Intersection&& other);
+    ~Intersection();
 private:
-    Readinput readinput;
-    static map<string, double> inputDict;
+    void checkCarSpawn();
+    double random();
+    int assignVehicle();
+    std::map<std::string, double> inputDict;
+    // Member variables
+    std::vector<VehicleBase *> westbound;
+    std::vector<VehicleBase *> eastbound;
+    std::vector<VehicleBase *> southbound;
+    std::vector<VehicleBase *> northbound;
     double halfsize;
     double maximum_simulated_time;
     double green_east_west;
@@ -27,21 +42,10 @@ private:
     double proportion_right_turn_SUVs;
     double proportion_right_turn_cars;
     double proportion_right_turn_trucks;
-
     double yellow_east_west;
     double yellow_north_south;
-
     double proportion_left_turn_SUVs;
     double proportion_left_turn_cars;
     double proportion_left_turn_trucks;
-
-    void checkCarSpawn();
-    int assignVehicle();
-public:
-    Intersection();
-    Intersection(const Intersection& other);
-    Intersection& operator=(Intersection& other);
-    Intersection(Intersection&& other);
-    Intersection& operator=(Intersection&& other);
-    ~Intersection();
+    ReadInput readinput;
 };
