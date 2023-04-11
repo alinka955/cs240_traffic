a9640941e5ba504bb31f59db6c3743c64484ae93
QED29 <50962562+QED29@users.noreply.github.com>
2023-04-11 15:32:38 -0400

    Update Random.cpp

--- Random.cpp
+++ Random.cpp
@@ -1,24 +1,18 @@
-#include <iostream>
-#include <random>
-#include <string>
 
-using namespace std;
+#include "RandomClass.h"
 
-class Random{
-
-        private:
-             int initialSeed;
-             mt19937 rng;
-        public Random::Random(){
+        RandomClass::RandomClass(){
         }
-        public Random::Random(int seed){
+
+        RandomClass::RandomClass(int seed){
              initialSeed = seed;
              uniform_real_distribution<double> rand_double(0.0, 1.0);
-             rng.seed(intialSeed);
+             rng.seed(initialSeed);
         }
 
-        public ~Random::Random(){
-             delete[] rng;
-             delete[] initialSeed;
+        RandomClass::~RandomClass(){
         }
-}
+
+        int main(){
+             return 0;
+        }
