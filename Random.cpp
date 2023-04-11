0e860c8c208538dc1df68c3d95014fe821abbe1c
QED29 <50962562+QED29@users.noreply.github.com>
2023-04-11 15:11:41 -0400

    Update Random.cpp

--- Random.cpp
+++ Random.cpp
@@ -9,14 +9,15 @@ class Random{
         private:
              int initialSeed;
              mt19937 rng;
-
-        public Random(int seed){
+        public Random::Random(){
+        }
+        public Random::Random(int seed){
              initialSeed = seed;
              uniform_real_distribution<double> rand_double(0.0, 1.0);
              rng.seed(intialSeed);
         }
 
-        public ~Random{
+        public ~Random::Random(){
              delete[] rng;
              delete[] initialSeed;
         }
