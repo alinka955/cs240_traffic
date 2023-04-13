39f5b3698d89a4c188596ae8fe4a64dfeee3ef64
danielkatz19 <98559516+danielkatz19@users.noreply.github.com>
2023-04-12 20:28:03 -0400

    Update VehicleBase.cpp

--- VehicleBase.cpp
+++ VehicleBase.cpp
@@ -5,18 +5,55 @@
 
 int VehicleBase::vehicleCount = 0;
 
+
+//intializes
 VehicleBase::VehicleBase(VehicleType type, Direction direction)
     : vehicleID(VehicleBase::vehicleCount++), 
       vehicleType(type),
       vehicleDirection(direction)
 {}
 
+//copy constructor
 VehicleBase::VehicleBase(const VehicleBase& other)
     : vehicleID(other.vehicleID),
       vehicleType(other.vehicleType),
       vehicleDirection(other.vehicleDirection)
 {}
 
+//destructor
 VehicleBase::~VehicleBase() {}
+
+//copy assignment
+VehicleBase& VehicleBase::operator=(const VehicleBase& other){
+    if (this == &other){
+      return *this;
+    }
+    vehicleID = other.vehicleID;
+    vehicleType = other.vehicleType;
+    vehicleDirection = other.vehicleDirection;
+    return *this;
+}
+
+//move constructor
+VehicleBase::VehicleBase(VehicleBase&& other)noexcept
+    : 
+    vehicleID(other.vehicleID),
+    vehicleType(other.vehicleType),
+    vehicleDirection(other.vehicleDirection){
+        other.vehicleID = 0;
+    }
+
+//move assignment
+VehicleBase& VehicleBase::operator=(VehicleBase&& other)noexcept{
+    if (this == &other) {
+        return *this;
+    }
+    vehicleID = other.vehicleID;
+    vehicleType = other.vehicleType;
+    vehicleDirection = other.vehicleDirection;
+    other.vehicleID = 0;
+    return *this;
+}
+
 
 #endif
