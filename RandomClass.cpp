e8320bdc128753e931b616d8ef6c36e26c712732
QED29 <50962562+QED29@users.noreply.github.com>
2023-04-20 16:18:57 -0400

    Update RandomClass.cpp

--- RandomClass.cpp
+++ RandomClass.cpp
@@ -1,15 +1,23 @@
 #include "RandomClass.h"
-
+        
+        /**
+        * This program is an instance of the Random class that generates 
+        * random double values between 0.0 and 1.0. 
+        */
+        //default constructor
         RandomClass::RandomClass(){
         }
 
+        //constructor that receives a seed input parameter
         RandomClass::RandomClass(int seed){
              initialSeed = seed;
              rng.seed(initialSeed);
         }
-
+        
+        //destructor
         RandomClass::~RandomClass(){
         }
+        //getter method, creates one random variable 
         double RandomClass::getRandom(){
              uniform_real_distribution<double> rand_double(0.0, 1.0);
              return rand_double(rng);
